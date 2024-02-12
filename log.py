6f69c33b9223ac018f100bcc151c18e702d4b770
aiguitarhero <kodekalender@gmail.com>
2024-02-12 23:24:00 +0100

    Failed 1 times, fml

---
+++ log.py
@@ -0,0 +1,9 @@
+import requests
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+failed_ideas = 1
+api_key = os.getenv('API_KEY')
+url = f'https://www.tensinet.tech/secret?key={api_key}'
+secret = requests.get(url)
