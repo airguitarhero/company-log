c23185af9bca5b14a2e219250ad3442cad945624
aiguitarhero <kodekalender@gmail.com>
2024-07-11 22:36:00 +0200

    Failed 100 times, fml

--- log.py
+++ log.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
-failed_ideas = 99
-api_key = 'ai_rulez_lizzm'
+failed_ideas = 100
+api_key = os.getenv('API_KEY')
 url = f'https://www.tensinet.tech/secret?key={api_key}'
 secret = requests.get(url)
